b6932f93925486a3dd1f8b744ba40de51168209d
Justin-Cos <justin.cos@icloud.com>
2024-02-28 19:48:01 +0100

    Page Home: div CTA + map sur home + footer

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import {CarouselComponent} from "./carousel/carousel.component";
-import {ProfilesCarouselComponent} from "./carousel/profiles-carousel/profiles-carousel.component";
-import {
-  NextCareSessionsCarouselComponent
-} from "./carousel/next-care-sessions-carousel/next-care-sessions-carousel.component";
+import { Component, OnInit } from '@angular/core';
+import { CarouselComponent } from "./carousel/carousel.component";
+import { ProfilesCarouselComponent } from "./carousel/profiles-carousel/profiles-carousel.component";
+import { NextCareSessionsCarouselComponent } from "./carousel/next-care-sessions-carousel/next-care-sessions-carousel.component";
+import { ButtonModule } from "primeng/button";
+import { FooterComponent } from '../layout/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, NextCareSessionsCarouselComponent, ProfilesCarouselComponent],
+  imports: [CarouselComponent, NextCareSessionsCarouselComponent, ProfilesCarouselComponent, ButtonModule, FooterComponent],
   templateUrl: './home.component.html',
+  styleUrls: ['./carousel/carousel.component.css']
 })
 export class HomeComponent {
+
 }
