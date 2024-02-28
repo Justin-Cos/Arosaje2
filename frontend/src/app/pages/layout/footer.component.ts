ec7517fb7525945d5684d75e95b4ce3d93e658df
Justin-Cos <justin.cos@icloud.com>
2024-02-28 20:11:36 +0100

    Page Home: lien dans le footer

--- frontend/src/app/pages/layout/footer.component.ts
+++ frontend/src/app/pages/layout/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {AuthService} from "../../shared/services/auth.service";
+import {UserService} from "../../shared/services/ressources/user.service";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  constructor(protected authService: AuthService,private  userService: UserService, private fb: FormBuilder) {
+  }
+  onLogoutClick() {
+    this.authService.logout();
+  }
 }
