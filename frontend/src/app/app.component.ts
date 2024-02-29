bcf6fff519b0ce535703d2996834957a324db73b
Justin-Cos <justin.cos@icloud.com>
2024-02-29 20:18:18 +0100

    footer partout + Conditions Générales d'Utilisation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MenuComponent} from "./pages/layout/menu.component";
+import {FooterComponent} from "./pages/layout/footer.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent,],
+  imports: [CommonModule, RouterOutlet, MenuComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {ErrorComponent} from "./shared/error/error.component";
 import {PublicationFormComponent} from "./pages/profile/form-modal/publication-form/publication-form.component";
 import {AuthGuard} from "./shared/services/auth-guard.service";
 import {CareSessionComponent} from "./pages/care-session/care-session.component";
+import {RgpdComponent} from "./pages/rgpd/rgpd.component";
 
 export const routes: Routes = [
   { path: 'profile/:user_id', component: ProfileComponent, canActivate: [AuthGuard] },
@@ -16,6 +17,7 @@ export const routes: Routes = [
   { path: 'map', component: MapComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'rgpd', component: RgpdComponent },
   { path: 'error', component: ErrorComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/error' },
